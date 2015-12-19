9130db63e7862b5e8eced13d5dfcdf0047f06b6c
Антон Кияткин <a.kiyatkin@itlf.ru>
2015-12-19 14:00:57 +0400

    infra config

--- css.js
+++ css.js
@@ -1,11 +1,12 @@
-//Свойство css
-
+define('?layer-css/css.js', ['?-event/event.js'], function () {
+	//Свойство css
 	infra.listen(infra,'layer.onparse',function(){
 		
 	});
 	infrajs.csscheck=function(layer){
-			if(!layer.css)return;
-			infra.fora(layer.css,function(css){
-				infra.loadCSS(css);
-			});
-		}
+		if(!layer.css)return;
+		infra.fora(layer.css,function(css){
+			infra.loadCSS(css);
+		});
+	}
+});
