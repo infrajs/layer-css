dce8cd2d869c2e0eea8c94707f125d71f753dea5
Антон Кияткин <a.kiyatkin@itlf.ru>
2015-12-20 21:01:23 +0400

    config::getAll

--- infra.php
+++ infra.php
@@ -3,9 +3,9 @@ namespace infrajs\layer\css;
 use infrajs\controller\Layer;
 use infrajs\event\Event;
 use infrajs\path\Path;
-use infrajs\infra\Infra;
+use infrajs\infra\Config;
 
-Infra::req('controller');
+Config::get('controller');
 
 Event::handler('layer.onshow', function (&$layer) {
 	if (Layer::pop($layer, 'onlyclient')) return;
